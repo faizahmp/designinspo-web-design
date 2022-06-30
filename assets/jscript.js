cca4664868297c4e6dbe25dae71a74cb18f2bfcc
Faizah Mappanyompa <72326639+faizahmp@users.noreply.github.com>
2022-06-30 19:58:22 +0800

    added files

---
+++ assets/jscript.js
@@ -0,0 +1,16 @@
+function order() {
+  alert("Sorry the stock isn't available at the moment!");
+}
+
+function email() {
+  alert("Input Your Email for the latest news from us!");
+}
+
+let navbar = document.getElementById("navbar");
+
+function showNav() {
+  navbar.style.right = "0";
+}
+function hideNav() {
+  navbar.style.right = "-200px";
+}
